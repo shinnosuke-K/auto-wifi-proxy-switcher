c2e022576eb99fc919b130d6679865cc7dfaba4c
shinnosuke-K <shin.050195@gmail.com>
2019-04-19 12:29:21 +0900

    add export and unset export

--- bin/auto-switch.sh
+++ bin/auto-switch.sh
@@ -21,11 +21,17 @@ if /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resou
   git config --global https.proxy $HTTPS_PROXY_NAME
   git config --global url."https://".insteadOf git://
 
+  export http_proxy=$HTTP_PROXY_NAME
+  export https_proxy=$HTTPS_PROXY_NAME
+
 elif /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s | grep -sq "$PRI_SSID"; then
   scselect $PRI_NETWORK
   git config --global --unset http.proxy
   git config --global --unset https.proxy
   git config --global --unset url."https://".insteadOf
+
+  unset http_proxy
+  unset https_proxy
 
 else
   scselect Automatic
